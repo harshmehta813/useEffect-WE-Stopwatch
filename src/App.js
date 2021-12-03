a57bf7d3c8435fd0bd6e23f713e33b6b83c85844
Harsh Mehta <87087018+harshmehta813@users.noreply.github.com>
2021-12-03 16:36:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import "./styles.css";
+import React, { useEffect, useRef, useState } from "react";
+
+export default function App() {
+  const [state, setState] = useState(0);
+
+  const timerRef = useRef(null);
+
+  useEffect(() => {
+    return pauseTimer;
+  }, []);
+
+  const startTimer = () => {
+    if (timerRef.current === null)
+      timerRef.current = setInterval(() => {
+        setState((prev) => prev + 1);
+      }, 1000);
+  };
+
+  const pauseTimer = () => {
+    clearInterval(timerRef.current);
+    timerRef.current = null;
+  };
+
+  const resetTimer = () => {
+    pauseTimer();
+    setState(0);
+  };
+
+  return (
+    <div className="App">
+      <h1>Stopwatch</h1>
+      <h2>{state}</h2>
+      <div>
+        <button onClick={startTimer}>START</button>
+        <button onClick={pauseTimer}>PAUSE</button>
+        <button onClick={resetTimer}>RESET</button>
+      </div>
+    </div>
+  );
+}
